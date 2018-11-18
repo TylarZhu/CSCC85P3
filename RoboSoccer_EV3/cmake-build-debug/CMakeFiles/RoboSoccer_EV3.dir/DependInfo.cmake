65f3383c03b740e50a27aa2754fc985b70efaa24
TylarZhu <zhuxingyuan123@gmail.com>
2018-11-18 06:30:55 -0800

    first commit

---
+++ RoboSoccer_EV3/cmake-build-debug/CMakeFiles/RoboSoccer_EV3.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/src/API/btcomm.c" "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/cmake-build-debug/CMakeFiles/RoboSoccer_EV3.dir/src/API/btcomm.c.o"
+  "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/src/API/btcomm_test.c" "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/cmake-build-debug/CMakeFiles/RoboSoccer_EV3.dir/src/API/btcomm_test.c.o"
+  "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/src/imagecapture/avilib.c" "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/cmake-build-debug/CMakeFiles/RoboSoccer_EV3.dir/src/imagecapture/avilib.c.o"
+  "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/src/imagecapture/color.c" "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/cmake-build-debug/CMakeFiles/RoboSoccer_EV3.dir/src/imagecapture/color.c.o"
+  "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/src/imagecapture/gui.c" "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/cmake-build-debug/CMakeFiles/RoboSoccer_EV3.dir/src/imagecapture/gui.c.o"
+  "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/src/imagecapture/imageCapture.c" "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/cmake-build-debug/CMakeFiles/RoboSoccer_EV3.dir/src/imagecapture/imageCapture.c.o"
+  "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/src/imagecapture/imageProc.c" "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/cmake-build-debug/CMakeFiles/RoboSoccer_EV3.dir/src/imagecapture/imageProc.c.o"
+  "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/src/imagecapture/luvcview.c" "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/cmake-build-debug/CMakeFiles/RoboSoccer_EV3.dir/src/imagecapture/luvcview.c.o"
+  "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/src/imagecapture/showdump.m" "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/cmake-build-debug/CMakeFiles/RoboSoccer_EV3.dir/src/imagecapture/showdump.m.o"
+  "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/src/imagecapture/svdDynamic.c" "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/cmake-build-debug/CMakeFiles/RoboSoccer_EV3.dir/src/imagecapture/svdDynamic.c.o"
+  "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/src/imagecapture/utils.c" "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/cmake-build-debug/CMakeFiles/RoboSoccer_EV3.dir/src/imagecapture/utils.c.o"
+  "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/src/imagecapture/v4l2uvc.c" "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/cmake-build-debug/CMakeFiles/RoboSoccer_EV3.dir/src/imagecapture/v4l2uvc.c.o"
+  "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/src/roboAI.c" "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/cmake-build-debug/CMakeFiles/RoboSoccer_EV3.dir/src/roboAI.c.o"
+  "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/src/roboSoccer.c" "/home/xingyuanzhu/Desktop/CSCC85/project/p3/RoboSoccer_EV3/cmake-build-debug/CMakeFiles/RoboSoccer_EV3.dir/src/roboSoccer.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../include"
+  "../src"
+  "../src/API"
+  "../src/imagecapture"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ RoboSoccer_EV3/cmake-build-debug/CMakeFiles/RoboSoccer_EV3.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for RoboSoccer_EV3.
+# This may be replaced when dependencies are built.
